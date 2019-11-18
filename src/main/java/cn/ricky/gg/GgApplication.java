ccfa61d2da0b9c736e8fba9ec3f09f3f308514cd
Ricky <fathernima@outlook.com>
2019-11-18 15:51:02 +0800

    test1

--- src/main/java/cn/ricky/gg/GgApplication.java
+++ src/main/java/cn/ricky/gg/GgApplication.java
@@ -2,6 +2,9 @@ package cn.ricky.gg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class GgApplication {
@@ -9,5 +12,7 @@ public class GgApplication {
     public static void main(String[] args) {
         SpringApplication.run(GgApplication.class, args);
     }
+
+
 
 }
--- src/main/java/cn/ricky/gg/hello.java
+++ src/main/java/cn/ricky/gg/hello.java
@@ -1,11 +1,14 @@
 package cn.ricky.gg;
 
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class hello {
-    @RequestMapping("/hello")
-    public String sayHello(){return "hello";}
+    @RequestMapping("/hello/{name}")
+    String Hello(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
 }
