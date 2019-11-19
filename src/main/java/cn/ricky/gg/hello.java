0bce11808e805a8e3259b8734201cb52c8955bea
Ricky <fathernima@gmail.com>
2019-11-20 00:15:43 +0800

    test2

--- src/main/java/cn/ricky/gg/hello.java
+++ src/main/java/cn/ricky/gg/hello.java
@@ -3,12 +3,25 @@ package cn.ricky.gg;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class hello {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+
     @RequestMapping("/hello/{name}")
     String Hello(@PathVariable String name) {
         return "Hello, " + name + "!";
+    }
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
     }
 }
