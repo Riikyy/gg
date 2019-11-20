9b4efe570412eb17684cb4bad5f47fb60c3f7553
Ricky <fathernima@gmail.com>
2019-11-20 22:53:16 +0800

    test2

--- src/main/java/cn/ricky/gg/GgApplication.java
+++ src/main/java/cn/ricky/gg/GgApplication.java
@@ -2,11 +2,13 @@ package cn.ricky.gg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableScheduling
 public class GgApplication {
 
     public static void main(String[] args) {
