a3dc94d1614f8f5a4e64f7207df248b0e0b1b3f2
Ricky <fathernima@outlook.com>
2019-11-25 16:09:10 +0800

    test1

--- src/main/java/cn/ricky/gg/mapper/StudentMapper.java
+++ src/main/java/cn/ricky/gg/mapper/StudentMapper.java
@@ -3,10 +3,12 @@ package cn.ricky.gg.mapper;
 import cn.ricky.gg.pojo.Student;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Mapper
+@Component("StudentMapper")
 public interface StudentMapper {
     @Select("select * from student")
     List<Student> findAll();
